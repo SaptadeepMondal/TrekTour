88bbd1104a704748830511140d9c68732917dd77
Saptadeep Mondal <saptadeepmondal.2005@gmail.com>
2026-09-14 03:39:45 +0530

    fix: remove sqlite folder creation and add pymongo srv

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 import os
 from dotenv import load_dotenv
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-INSTANCE_PATH = os.path.join(BASE_DIR, "instance")
-os.makedirs(INSTANCE_PATH, exist_ok=True)
 
 load_dotenv()
 
